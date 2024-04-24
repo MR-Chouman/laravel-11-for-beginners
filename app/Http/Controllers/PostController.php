72b1bab35535bb6eeee443bd5e46be99850848ac
MR-Chouman <choumanmohamadrida@gmail.com>
2024-04-24 16:14:44 +0300

    <BE> Updated Show Method in Post Model to Eloquent Style

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,9 +7,8 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function show($postId)
+    public function show(Post $post)
     {
-        $post = Post::find($postId);
         return view('post', compact('post'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::view('contact-us', 'contact')->name('contact-us');
 Route::view('about-us', 'about')->name('about-us');
-Route::get('/posts/{postId}', [\App\Http\Controllers\PostController::class, 'show'])->name('post.show');
+Route::get('/posts/{post}', [\App\Http\Controllers\PostController::class, 'show'])->name('post.show');
 
