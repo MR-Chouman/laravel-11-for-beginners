851d77543b1823dbafad736b4c566555f50a58bf
MR-Chouman <choumanmohamadrida@gmail.com>
2024-04-24 14:21:21 +0300

    <BE> Created Category Model

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -107,18 +107,8 @@
                                         @foreach($categories as $category)
                                             <li><a href="#!">{{ $category->Name }}</a></li>
                                         @endforeach
-                                        <!-- <li><a href="#!">Web Design</a></li>
-                                        <li><a href="#!">HTML</a></li>
-                                        <li><a href="#!">Freebies</a></li> -->
                                     </ul>
                                 </div>
-                                <!-- <div class="col-sm-6">
-                                    <ul class="list-unstyled mb-0">
-                                        <li><a href="#!">JavaScript</a></li>
-                                        <li><a href="#!">CSS</a></li>
-                                        <li><a href="#!">Tutorials</a></li>
-                                    </ul>
-                                </div> -->
                             </div>
                         </div>
                     </div>
