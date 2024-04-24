30bfb6cc38af3c507634b47f28d9df0864996e7d
MR-Chouman <choumanmohamadrida@gmail.com>
2024-04-24 13:48:19 +0300

    <BE> Updated Route of Home Page to Controller

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'home')->name('home');
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::view('contact-us', 'contact')->name('contact-us');
 Route::view('about-us', 'about')->name('about-us');
 
