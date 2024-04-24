1c7cd03c26ffe315cce13ba99280707ce36815cd
MR-Chouman <choumanmohamadrida@gmail.com>
2024-04-24 15:43:06 +0300

    <BE> Updated Home Controller to Get Posts by Requested Category ID

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,7 +12,7 @@ class HomeController extends Controller
     public function index()
     {
         $categories = Category::all();
-        $posts = Post::latest()->get();
+        $posts = Post::where('category_id', request('category_id'))->latest()->get();
         return view('home', compact('categories', 'posts'));
     }
 }
