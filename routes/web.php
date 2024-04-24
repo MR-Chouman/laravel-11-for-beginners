c3c8feebe40a221654a7f7ef200ac4347e2142b4
MR-Chouman <choumanmohamadrida@gmail.com>
2024-04-24 10:51:16 +0300

    <BE> Applied Shorter Syntax for Routes

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
 
-Route::get('/second', function () {
-    return view('second');
-});
+Route::view('/second', 'second');
